91a43f20f9d80290a0411f238e4ed91505cd061d
Dejan Vasic <dejanvasic24@gmail.com>
2023-03-18 21:58:34 +1100

    Moving to Main file for chapter two

--- src/main/kotlin/chapter_two/Composition.kt
+++
@@ -1,9 +0,0 @@
-package chapter_two
-
-fun main() {
-    val composer = compose<Int, Int, Int>({ a -> 1 + a }, { b -> 1 + b })
-    println("composer plus ${composer(5)}")
-}
-
-fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
-    { a: A -> f(g(a)) }
--- src/main/kotlin/chapter_two/Main.kt
+++ src/main/kotlin/chapter_two/Main.kt
@@ -6,6 +6,9 @@ fun main() {
 
     val plus = uncurry<Int, Int, Int> { a -> { b -> a + b } }
     println("chapter_two.uncurry plus: ${plus(1, 2)}")
+
+    val composer = compose<Int, Int, Int>({ a -> 1 + a }, { b -> 1 + b })
+    println("composer plus ${composer(5)}")
 }
 
 fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
@@ -13,3 +16,6 @@ fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
 
 fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C =
     { a: A, b: B -> f(a)(b) }
+
+fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
+{ a: A -> f(g(a)) }
