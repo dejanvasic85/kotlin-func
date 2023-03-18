781dbfc47636b48ddc9bcfd3be04e572ec96d5d7
Dejan Vasic <dejanvasic24@gmail.com>
2023-03-18 21:56:09 +1100

    Moving to Main file for chapter one

--- src/main/kotlin/chapter_one/Main.kt
+++ src/main/kotlin/chapter_one/Main.kt
@@ -1,6 +1,8 @@
 package chapter_one
 
 fun main() {
+    println("Fib result: ${fib(10)}")
+
     val items = listOf(1, 2, 3, 4, 5)
     println("is sorted: ${isSorted(items) { a, b -> a < b }}")
     println("is sorted: ${isSorted(listOf('z', 'y', 'x')) { a, b -> a > b }}")
@@ -20,3 +22,12 @@ fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean {
     }
     return loop(aa.head, aa.tail)
 }
+
+fun fib(nth: Int): Int {
+    tailrec fun go(idx: Int, prev: Int, acc: Int): Int =
+        if (idx == 0) acc
+        else go(idx - 1, acc, prev + acc)
+
+    return go(nth, 0, 1)
+}
+
