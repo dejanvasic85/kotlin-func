c67ef7069d611c2575226781556e2a4bce4112b3
Dejan Vasic <dejanvasic24@gmail.com>
2023-12-18 11:44:10 +1100

    Adding some stream methods with tests

--- src/main/kotlin/me/dejanvasic/Stream.kt
+++ src/main/kotlin/me/dejanvasic/Stream.kt
@@ -8,12 +8,83 @@ data class ConsStream<out A>(
 
 object EmptyStream : Stream<Nothing>()
 
-fun <A> Stream<A>.headOption(): Option<A> =
-    when (this) {
-        is EmptyStream -> None
-        is ConsStream -> Some(head())
-    }
 
 sealed class Stream<out A> {
+    companion object {
+        fun <A> of(vararg xs: A): Stream<A> {
+            return if (xs.isEmpty()) emptyStream()
+            else consStream({ xs[0] }, { of(*xs.sliceArray(1 until xs.size)) })
+        }
+    }
+}
 
+fun <A> consStream(hd: () -> A, tl: () -> Stream<A>): Stream<A> {
+    val head: A by lazy(hd)
+    val tail: Stream<A> by lazy(tl)
+    return ConsStream({ head }, { tail })
+}
+
+fun <A> emptyStream(): Stream<A> = EmptyStream
+
+fun <A> Stream<A>.headOption(): Option<A> = when (this) {
+    is EmptyStream -> None
+    is ConsStream -> Some(head())
+}
+
+fun <A> Stream<A>.toList(): LinkedList<A> {
+    tailrec fun go(xs: Stream<A>, acc: LinkedList<A>): LinkedList<A> {
+        return when (xs) {
+            is EmptyStream -> acc
+            is ConsStream -> go(xs.tail(), Cons(xs.head(), acc))
+        }
+    }
+    return LinkedList.reverse(go(this, Nil))
+}
+
+fun <A> Stream<A>.take(n: Int): Stream<A> {
+    tailrec fun go(xs: Stream<A>, n: Int): Stream<A> {
+        return when (xs) {
+            is EmptyStream -> emptyStream()
+            is ConsStream ->
+                if (n == 0) emptyStream()
+                else consStream(xs.head, { go(xs.tail(), n - 1) })
+        }
+    }
+    return go(this, n)
+}
+
+fun <A> Stream<A>.drop(n: Int): Stream<A> {
+    tailrec fun go(xs: Stream<A>, n: Int): Stream<A> {
+        return when (xs) {
+            is EmptyStream -> emptyStream()
+            is ConsStream ->
+                if (n == 0) xs
+                else go(xs.tail(), n - 1)
+        }
+    }
+    return go(this, n)
+}
+
+fun <A> Stream<A>.takeWhile(p: (A) -> Boolean): Stream<A> =
+    foldRight({ emptyStream() }) { a, b -> if (p(a)) consStream({ a }, b) else b() }
+
+fun <A> Stream<A>.length(): Int {
+    tailrec fun go(xs: Stream<A>, count: Int): Int {
+        return when (xs) {
+            is EmptyStream -> count
+            is ConsStream -> go(xs.tail(), count + 1)
+        }
+    }
+    return go(this, 0)
+}
+
+tailrec fun <A, B> Stream<A>.foldRight(z: () -> B, f: (A, () -> B) -> B): B {
+    return when (this) {
+        is EmptyStream -> z()
+        is ConsStream -> f(this.head()) { this.tail().foldRight(z, f) }
+    }
+}
+
+fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean {
+    return foldRight({ true }) { a, b -> p(a) && b() }
 }
--- src/test/kotlin/me/dejanvasic/StreamTest.kt
+++ src/test/kotlin/me/dejanvasic/StreamTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.DescribeSpec;
 import io.kotest.matchers.shouldBe
 
 class StreamTest : DescribeSpec({
-    describe("head") {
+    describe("headOption") {
         it("should return the head value when there is an item") {
             val stream = ConsStream({ 1 }, { EmptyStream })
 
@@ -19,6 +19,91 @@ class StreamTest : DescribeSpec({
             val head = stream.headOption()
 
             head shouldBe None
+        }
+    }
+
+    describe("length") {
+        it("should return the number of items in the stream") {
+            val items = Stream.of(1, 1, 1)
+
+            items.length() shouldBe 3
+        }
+    }
+
+    describe("of") {
+        it("should create a stream of numbers") {
+            val stream = Stream.of(1, 2)
+
+            val result = stream.headOption()
+
+            result shouldBe Some(1)
+        }
+    }
+
+    describe("toList") {
+        it("should return a list from a stream") {
+            val stream = Stream.of("one", "two", "three")
+
+            val result = stream.toList()
+
+            result shouldBe Cons("one", Cons("two", Cons("three", Nil)))
+        }
+    }
+
+    describe("take") {
+        it("should return the first item") {
+            val stream = Stream.of(1, 2)
+
+            val first = stream.take(1)
+
+            first.headOption() shouldBe Some(1)
+        }
+    }
+
+    describe("drop") {
+        it("should return the last element by skipping the first two") {
+            val stream = Stream.of(
+                1, 2, 3
+            )
+
+            val dropped = stream.drop(2)
+
+            dropped.headOption() shouldBe Some(3)
+        }
+    }
+
+    describe("takeWhile") {
+        it("should return the values that meet the predicate criteria") {
+            val stream = Stream.of(1, 2, 3, 4, 5).takeWhile { a -> a % 2 == 0 }
+
+            stream.length() shouldBe 2
+            stream.headOption() shouldBe Some(2)
+        }
+    }
+
+    describe("foldRight") {
+        it("should reduce the stream by adding all the values together") {
+            val stream = Stream.of(1, 2, 3)
+
+            val sum = stream.foldRight({ 0 }) { a, b -> a + b() }
+
+            sum shouldBe 6
+        }
+    }
+
+    describe("forAll") {
+        it("should return true when all items meet the criteria") {
+            val stream = Stream.of(1, 2, 3)
+            val areAllGreaterThanZero = stream.forAll { a -> a > 0 }
+
+            areAllGreaterThanZero shouldBe true
+        }
+
+        it("should return false when one items is false") {
+            val stream = Stream.of(1, 2, 3)
+            val areAllOne = stream.forAll { a -> a == 1 }
+
+            areAllOne shouldBe false
         }
     }
 })
